4f2c4ffa4ec4c4443b851edd777681a6e3653b03
Alex Bush <alexabush7@gmail.com>
2018-06-05 20:31:14 -0700

    added jest test to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { isConnectFour } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+const arrayWithWin = [0, 1, 2, 1, 1, 1, 1];
+const arrayWithNoWin = [0, 1, 2, 1, 2, 1, 1];
+
+test('array with 4 in a row returns true', () => {
+  expect(isConnectFour(arrayWithWin)).toBe(true);
+  expect(isConnectFour(arrayWithNoWin)).toBe(false);
 });
